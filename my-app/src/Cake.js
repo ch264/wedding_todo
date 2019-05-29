c5cd367380474ea229e9cae9c8caa17a2455ec03
ch264 <christinavhastenrath@gmail.com>
2019-05-29 13:38:00 -0700

    added a ternary operator in the cake file to show conditional data

--- my-app/src/Cake.js
+++ my-app/src/Cake.js
@@ -5,16 +5,27 @@ import React from 'react';
 const Cake = (props) => {
 	// destructuring props to variables
 	const { cake } = props;
-	// cycle through cakes array and map to new cakeList array
+	// // cycle through cakes array and map to new cakeList array
+	// const cakeList = cake.map(cake => {
+	
+	// 	return (
+	// 		<div className="Cake" key={ cake.id}>
+	// 			<div>Baker</div>
+	// 			<div>Budget</div>
+	// 			<div>Cover: { cake.name }</div>
+	// 		</div>
+	// 	)
+	// })
+
 	const cakeList = cake.map(cake => {
-		return (
-			<div className="Cake" key={ cake.id}>
-				<div>Baker</div>
-				<div>Budget</div>
-				<div>Cover: { cake.name }</div>
-			</div>
-		)
-	})
+		return cake.id >1 ? (
+				<div className="Cake" key={ cake.id}>
+					<div>Baker</div>
+					<div>Budget</div>
+					<div>Cover: { cake.name }</div>
+	 			</div>
+		) : null
+	});
 	return (
 	<div className="cake-list">
 		{ cakeList }
