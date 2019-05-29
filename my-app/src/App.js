ca81a920033d4b9402b0b124ba6a455dd6cdfd64
ch264 <christinavhastenrath@gmail.com>
2019-05-29 10:50:57 -0700

    setup react app folder structure

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+// Root component
+
+import React from 'react';
+import Cake from './Cake';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome</h1>
+      <Cake />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/Cake.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import App from './App';
+
+class Cake extends Component {
+	render() {
+		return (
+			<div className="cake">
+				<div>Baker</div>
+				<div>budget</div>
+			</div>
+		);
+	}
+}
+
+export default Cake;
